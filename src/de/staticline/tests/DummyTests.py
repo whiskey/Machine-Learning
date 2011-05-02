8c5cabcffcdd640fe71e6772e00877fa39b754e2
Carsten Witzke <whiskey@staticline.de>
2011-05-02 20:27:59 +0200

    who needs a main Module when you have unit tests? some (optional) output for the uni tests
    Signed-off-by: Carsten Witzke <whiskey@staticline.de>

--- src/de/staticline/tests/DummyTests.py
+++ src/de/staticline/tests/DummyTests.py
@@ -8,17 +8,18 @@ from de.staticline.classification.Dummys import Always1Predictor
 
 
 class Test(unittest.TestCase):
-
+    __verbose = True #print some stats?
 
     def testDummyModel(self):
         dummy = Always1Predictor()
         dummy.buildClassifier('../../../../data/a1a')
         dummy.validateModel('../../../../data/a1a.t')
-        print '# instances\n    training:{tr:d}\n    test:{te:d}'.format(
-            tr=dummy.get_inst_train(), te=dummy.get_inst_test())
-        print '% correct: {corr:2.3f} - RMSE: {rmse:.2f}'.format(
-            corr=dummy.get_pctCorrect(), rmse=dummy.get_rmse())
-        self.assertTrue(dummy.get_pctCorrect() > 24) # for data set a1a
+        if self.__verbose:
+            print '# instances\n    training:{tr:d}\n    test:{te:d}'.format(
+                                tr=dummy.get_inst_train(), te=dummy.get_inst_test())
+            print '% correct: {corr:2.3f} - RMSE: {rmse:.2f}'.format(
+                                corr=dummy.get_pctCorrect(), rmse=dummy.get_rmse())
+        self.assertTrue(dummy.get_pctCorrect() > 24) # for data set a1a !!!
 
 
 if __name__ == "__main__":
